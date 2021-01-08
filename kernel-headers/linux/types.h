aa3df8ea14587e3fb940b2bbb75bfec152138ab6
Jeong Seong Pil <jjjhitel@gmail.com>
2021-01-09 05:09:45 +0900

    picasso: kernel-headers: Modify types.h to be able to use from user space

--- kernel-headers/linux/types.h
+++ kernel-headers/linux/types.h
@@ -5,11 +5,6 @@
 #include <asm/types.h>
 
 #ifndef __ASSEMBLY__
-#ifndef	__KERNEL__
-#ifndef __EXPORTED_HEADERS__
-#warning "Attempt to use kernel headers from user space, see http://kernelnewbies.org/KernelHeaders"
-#endif /* __EXPORTED_HEADERS__ */
-#endif
 
 #include <linux/posix_types.h>
 
